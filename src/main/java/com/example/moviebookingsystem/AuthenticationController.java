ee3286e0eb44fccbc2d9a8b744e132b8f33dc099
sarahishamsaied <sarahishamsaied@gmail.com>
2022-05-15 01:43:55 +0200

    tested database & created signinPage#1

--- src/main/java/com/example/moviebookingsystem/AuthenticationController.java
+++ src/main/java/com/example/moviebookingsystem/AuthenticationController.java
@@ -2,8 +2,29 @@ package com.example.moviebookingsystem;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
+import java.sql.*;
 public class AuthenticationController {
+    Connection testConnection;
+    {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String user = "root";
+            String password = "";
+            testConnection = DriverManager.getConnection("jdbc:mysql://localhost:3307/moviebookingsystem",user,password);
+            Statement testStatement = testConnection.createStatement();
+            ResultSet testRes = testStatement.executeQuery("select * from users");
+            while(testRes.next()){
+                System.out.println(testRes.getString("name")+" "+testRes.getString("email"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        catch(Exception e){
+            System.out.println("cannot find class");
+            e.printStackTrace();
+        }
+    }
+
     @FXML
     void onSignUp(ActionEvent actionEvent) {
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.moviebookingsystem {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.moviebookingsystem to javafx.fxml;
     exports com.example.moviebookingsystem;
