5fc0679845b58c067b455e99713b59f940bda5c0
maryamnassar020202l <maryamnassar02@gmail.com>
2022-06-03 20:25:49 +0200

    added Add Movie Form- Movies Form

--- src/main/java/com/example/moviebookingsystem/HelloApplication.java
+++ src/main/java/com/example/moviebookingsystem/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MoviesForm.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1400, 650);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("HelloView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1022, 650);
         stage.setTitle("WhatTheVox!");
         stage.setScene(scene);
         stage.show();
---
+++ src/main/java/com/example/moviebookingsystem/TicketForm.java
@@ -0,0 +1,4 @@
+package com.example.moviebookingsystem;
+
+public class TicketForm {
+}
